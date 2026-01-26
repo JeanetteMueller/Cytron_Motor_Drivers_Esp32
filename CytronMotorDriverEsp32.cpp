35386a19f8dcd8a40756eca046dafe17c1f8057c
Jeanette Müller <themaverick@themaverick.de>
2026-01-26 14:42:52 +0100

    better defaults, automatic channel mapping

--- CytronMotorDriverEsp32.cpp
+++ CytronMotorDriverEsp32.cpp
@@ -1,71 +1,78 @@
 #include "CytronMotorDriverEsp32.h"
 #include <math.h>
 
-CytronMD::CytronMD(CytronMD_Motor_Mode mode, uint8_t pin1, uint8_t pin2, uint8_t channel, uint32_t freq, uint8_t resolution)
+uint8_t CytronMD::nextChannel = 0;
+
+CytronMD::CytronMD(CytronMD_Motor_Mode mode, uint8_t pin1, uint8_t pin2, uint32_t freq, uint8_t resolution)
 {
-      _mode = mode;
-      _pin1 = pin1;
-      _pin2 = pin2;
-      _channel = channel;
-      _maxSpeed = (pow(2, resolution) - 1);
+  _mode = mode;
+  _pin1 = pin1;
+  _pin2 = pin2;
+  _channel = nextChannel;  // set automatically
+  _maxSpeed = (pow(2, resolution) - 1);
 
-      pinMode(_pin1, OUTPUT);
-      pinMode(_pin2, OUTPUT);
+  pinMode(_pin1, OUTPUT);
+  pinMode(_pin2, OUTPUT);
 
-      switch (_mode)
-      {
-        case PWM_DIR:
-            ledcAttachChannel(_pin1, freq, resolution, channel);
-            ledcWriteChannel(channel, 0);
-            digitalWrite(_pin2, LOW);
-          break;
+  switch (_mode)
+  {
+  case PWM_DIR:
+    ledcAttachChannel(_pin1, freq, resolution, _channel);
+    ledcWriteChannel(_channel, 0);
+    digitalWrite(_pin2, LOW);
+    nextChannel++; // increment to next channel 
+    break;
 
-      case PWM_PWM:
+  case PWM_PWM:
 
-        ledcAttachChannel(_pin1, freq, resolution, channel);
-        ledcAttachChannel(_pin2, freq, resolution, channel * 10);
+    ledcAttachChannel(_pin1, freq, resolution, _channel);
+    ledcAttachChannel(_pin2, freq, resolution, _channel + 1);
 
-        ledcWriteChannel(channel, 0);
-        ledcWriteChannel(channel * 10, 0);
-      
-        break;
-      }
+    ledcWriteChannel(_channel, 0);
+    ledcWriteChannel(_channel + 1, 0);
+    nextChannel += 2;  // increment 2 channels
+    break;
+  }
+
+  Serial.print("   CytronMD::init done");
 }
 
 void CytronMD::setSpeed(int32_t speed)
 {
-  
   // Make sure the speed is within the limit.
   speed = constrain(speed, -_maxSpeed, _maxSpeed);
-  
+
   // Make sure the speed is within the limit.
   uint32_t setSpeed = abs(speed);
   setSpeed = constrain(setSpeed, 0, _maxSpeed);
-      
-      // Set the speed and direction.
-      switch (_mode)
-      {
-        case PWM_DIR:
-            ledcWrite(_channel, setSpeed);
 
-            if (speed >= 0) {
-              digitalWrite(_pin2, LOW);
-            }else {
-              digitalWrite(_pin2, HIGH);
-            }
-          break;
+  // Set the speed and direction.
+  switch (_mode)
+  {
+  case PWM_DIR:
+    ledcWrite(_channel, setSpeed);
 
-          case PWM_PWM:
-            if (speed >= 0)
-            {
-              ledcWriteChannel(_channel, setSpeed);
-              ledcWriteChannel(_channel * 10, 0);
-            }
-            else
-            {
-              ledcWriteChannel(_channel, 0);
-              ledcWriteChannel(_channel * 10, setSpeed);
-            }
-            break;
-      }
+    if (speed >= 0)
+    {
+      digitalWrite(_pin2, LOW);
+    }
+    else
+    {
+      digitalWrite(_pin2, HIGH);
+    }
+    break;
+
+  case PWM_PWM:
+    if (speed >= 0)
+    {
+      ledcWriteChannel(_channel, setSpeed);
+      ledcWriteChannel(_channel + 1, 0);
+    }
+    else
+    {
+      ledcWriteChannel(_channel, 0);
+      ledcWriteChannel(_channel + 1, setSpeed);
+    }
+    break;
+  }
 }
--- CytronMotorDriverEsp32.h
+++ CytronMotorDriverEsp32.h
@@ -6,29 +6,33 @@
 
 #if defined(ARDUINO_ARCH_ESP32)
 
-enum CytronMD_Motor_Mode {
+enum CytronMD_Motor_Mode
+{
   PWM_DIR,
   PWM_PWM,
 };
 
 class CytronMD
 {
-  public:
-    CytronMD(CytronMD_Motor_Mode mode, uint8_t pin1, uint8_t pin2, uint8_t channel = 0, uint32_t freq = 0, uint8_t resolution = 0);
-    void setSpeed(int32_t speed);
-    
-  protected:
-    CytronMD_Motor_Mode _mode;
-    
-  	uint8_t _pin1;
-    uint8_t _pin2;
-    uint8_t _channel;
-    int32_t _maxSpeed;
+public:
+  CytronMD(CytronMD_Motor_Mode mode, uint8_t pin1, uint8_t pin2, uint32_t freq = 5000, uint8_t resolution = 8);
+  void setSpeed(int32_t speed);
+
+private:
+  static uint8_t nextChannel; // Statischer Zähler
+
+protected:
+  CytronMD_Motor_Mode _mode;
+
+  uint8_t _pin1;
+  uint8_t _pin2;
+  uint8_t _channel;
+  uint32_t _maxSpeed;
 };
 
 #else
 
-//only compatible with ESP32
+// only compatible with ESP32
 
 #endif
 
